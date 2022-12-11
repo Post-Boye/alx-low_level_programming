fd0007bd68dc42e8154361217b24f0edd7d33662
Post-Boye <boyejustice64@gmail.com>
2022-12-11 01:28:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - main block
+ * Return: o
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
+
